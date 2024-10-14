2e55d6faec16539cf07df101767cee28fcef1d9f
Javier Gonzalez <javgon2306@gmail.com>
2024-10-14 12:40:06 -0300

    add ejs

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const app = express();
 
 const port = 3000;
 
+//motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', __dirname +'/views');
+
+//midleware
+app.use(express.static(__dirname + '/public'))
+
+// app.use((req, res, next) => {
+//     // res.status(404).sendFile(__dirname + "/public/404.html")
+// })
+
 app.get('/', (req, res) =>{
-    res.send('Respuesta del server')
+    res.render("index", {titulo: 'Ejemplo de Node JS'})
 })
 app.get('/servicio', (req, res) =>{
     res.send('Respuesta del server en la pagina de servicios')
 })
 
-app.use(express.static(__dirname + '/public'))
 
 app.listen(port, () => {
     console.log('Server running on port', port)
